0878d1d3aba941b40fb984c5510617c7e854263b
Nuzayer101 <kamalynuzayer@gmail.com>
2025-03-10 02:58:44 +0600

    React JS Mini Project-Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Input from "./components/Input";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="!bg-black h-screen p-3 w-[1350px]">
+      <div className="bg-white shadow-2xl min-h-[400px] max-w-[750px] mx-auto  rounded-2xl">
+        <h1 className="text-center  text-4xl uppercase mt-2 font-medium">to-do-app</h1>
+      <Input></Input>
+      </div>
+    </div>
 
-export default App
+  );
+};
+
+export default App;
