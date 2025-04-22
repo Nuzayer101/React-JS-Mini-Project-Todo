efbf3375fefe41c774c30ee17b76bb3ece557a80
Nuzayer101 <kamalynuzayer@gmail.com>
2025-04-23 01:18:17 +0600

    to-do-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import React from "react";
-import Input from "./components/Input";
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="!bg-black h-screen p-3 w-[1350px]">
-      <div className="bg-white shadow-2xl min-h-[400px] max-w-[750px] mx-auto  rounded-2xl">
-        <h1 className="text-center  text-4xl uppercase mt-2 font-medium">to-do-app</h1>
-      <Input></Input>
-      </div>
-    </div>
+    <div>App</div>
+  )
+}
 
-  );
-};
-
-export default App;
+export default App
